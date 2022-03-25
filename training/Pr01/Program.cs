4e9cf74efebf52ceb1c12ae45ad30892631ae42f
Spasov3618 <svetlio_spasov@abv.bg>
2022-03-25 16:03:29 +0200

    Add

---
+++ training/Pr01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Pr01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(new string(charArray));
+       
+        }
+    }
+}

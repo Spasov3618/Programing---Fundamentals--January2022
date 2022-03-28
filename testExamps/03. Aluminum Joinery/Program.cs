2d3459d31deda262cb028aa39b2b95d5dde21e6d
Spasov3618 <svetlio_spasov@abv.bg>
2022-03-28 15:26:32 +0300

    Add

---
+++ testExamps/03. Aluminum Joinery/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _03._Aluminum_Joinery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Фирмата предлага също и доставка на поръчките си срещу 60 лв.
+            
+            //Ако поръчката надвишава 99 броя  – върху крайната цена се начисляват допълнителни 4 % отстъпка
+            //(след като се начисли цената за доставка, ако има такава).
+            //При поръчка под 10 бр.на конзолата да бъде изписано "Invalid order"
+                      
+            //•	С доставка -"With delivery"
+            //•	Без доставка -"Without delivery"
+            //Изход:
+            //            Извежда се едно число – стойността на поръчката, в следния формат:
+            //            o   "{Обща стойност на поръчката} BGN"
+            //Резултатът да се форматира до втори знак след десетичната запетая.
+            int numWindows = int.Parse(Console.ReadLine());
+            string kindWindows = Console.ReadLine();
+            string delivery = Console.ReadLine();
+            double price = 0;
+            double totalPrice = 0;
+            if (numWindows<10)
+            {
+                Console.WriteLine("Invalid order");
+                return;
+            }
+            switch (kindWindows )
+            {
+                case "90X130":
+                    price =  110;
+                    if (numWindows >=30 && numWindows <=60)
+                    {
+                        price *= 0.95;
+
+                    }
+                    else if (numWindows >60)
+                    {
+                        price *= 0.92;
+                    }
+                    break;
+                case "100X150":
+                    price = 140;
+                    if (numWindows >=40 && numWindows <=80)
+                    {
+                        price *= 0.94;
+                    }
+                    else if (numWindows >80)
+                    {
+                        price *= 0.9;
+                    }
+                    break;
+                case "130X180":
+                    price = 190;
+                    if (numWindows >=20 && numWindows<=50)
+                    {
+                        price *= 0.93;
+                    }
+                    else if (numWindows >50)
+                    {
+                        price *= 0.88;
+                    }
+                    break;
+                case "200X300":
+                    price = 250;
+                    if (numWindows >=25 && numWindows <=50)
+                    {
+                        price *= 0.91;
+                    }
+                    else if (numWindows>50)
+                    {
+                        price *= 0.86;
+                    }
+                    break;
+            }
+            totalPrice = numWindows * price;
+            switch (delivery)
+            {
+                case "With delivery":
+                    totalPrice += 60;
+                    break;
+                    
+            }
+            if (numWindows >99)
+            {
+                totalPrice *= 0.96;
+            }
+            Console.WriteLine($"{totalPrice:f2} BGN");
+        }
+    }
+}
---
+++ testExamps1/test/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма, която проследява представянето на вашия отбор на благотворителен коледен турнир.
+            //Всеки ден получавате имена на игри до команда "Finish".
+            //Със спечелването на всяка една игра печелите по 20лв.за благотворителност.
+            //Трябва да изчислите колко пари сте спечелили на края на деня.
+            //Ако имате повече спечелени игри, отколкото загубени – вие сте победители този ден и увеличавате парите от него с 10 %.
+            //При приключване на турнира ако през повечето дни сте били победители печелите турнира и увеличавате всичките спечелени пари с 20 %.
+            //Никога няма да имате равен брой спечелени и загубени игри.
+            //Вход
+            //Първоначално от конзолата се прочита броя дни на турнира – цяло число в интервала[1… 20]
+            //До получаване на командата "Finish" се чете:
+            //•	Спорт  – текст
+            //За всеки спорт се прочита:
+            //o Резултат  – текст с възможности: "win" или "lose"
+            //Изход
+            //Накрая се отпечатва един ред:
+            //•	Ако сте спечелили турнира:
+            //     	"You won the tournament! Total raised money: {спечелените пари}"
+            //•	Ако сте загубили на турнира:
+            //            "You lost the tournament! Total raised money: {спечелените пари}"
+            //Парите да бъдат форматирани до втората цифра след десетичния знак
+            int day = int.Parse(Console.ReadLine());
+            double money = 0;
+            double moneyDay = 0;
+            int ollCounterWin = 0;
+            int ollCounterLost = 0;
+            for (int i = 1; i <= day; i++)
+            {
+                string nameGames = Console.ReadLine();
+                int counterWin = 0;
+                int counterLost = 0;
+                while (nameGames != "Finish")
+                {
+                    string rezult = Console.ReadLine();
+                    if (rezult == "win")
+                    {
+                        money += 20;
+                        counterWin++;
+                        ollCounterWin++;
+                    }
+                    else
+                    {
+                        counterLost++;
+                        ollCounterLost++;
+                    }
+
+
+
+
+
+                    nameGames = Console.ReadLine();
+
+                    if (nameGames == "Finish")
+                    {
+                        if (counterWin > counterLost)
+                            money += money * 0.10;
+                        moneyDay += money;
+                        money = 0;
+                    }
+
+                }
+            }
+                    if (ollCounterWin > ollCounterLost)
+                    {
+                        moneyDay  += moneyDay * 0.2;
+                        Console.WriteLine($"You won the tournament! Total raised money: {moneyDay:f2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You lost the tournament! Total raised money: {moneyDay:f2}");
+                    }
+               
+           
+
+        }
+    }
+}

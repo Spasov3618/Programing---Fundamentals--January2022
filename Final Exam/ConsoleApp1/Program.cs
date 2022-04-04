3b133d40e8a3c8c7c472b3304bf3ed378379736f
Spasov3618 <svetlio_spasov@abv.bg>
2022-04-04 12:18:53 +0300

    add final exam

---
+++ Final Exam/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = a++;
+            int c = ++a;
+            Console.WriteLine(c);
+        }
+    }
+}
